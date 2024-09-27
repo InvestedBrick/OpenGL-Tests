e00ae60cc15dc65110122b35cc2a95fc1d592146
InvestedBrick <julianbrecht25@gmail.com>
2024-09-27 21:18:19 +0200

    Fixed for triangle Rendering

--- Simple_API/Shapes/Circle.hpp
+++ Simple_API/Shapes/Circle.hpp
@@ -2,68 +2,72 @@
 #include "BaseShape.hpp"
 #include "../defines.hpp"
 #include <cmath>
-
 #define TWO_PI 6.2831853
 class Circle : public BaseShape{
 protected:
-
     float m_radius;
     uint m_segments;
     uint positions_size;
 public:
-    
-    Circle(const float x, const float y,const uint segments,const float radius = 1.f) : BaseShape(x, y), m_radius(radius) , m_segments(segments) {
+    Circle(const float x, const float y, const uint segments, const float radius = 1.f)
+        : BaseShape(x, y), m_radius(radius), m_segments(segments) {
+        assert (m_segments > 0 && "Number of segments must be >= 1");
         init_positions();
         init_indices();
         calc_positions();
-        positions_size = (m_segments + 2) * 2 * sizeof(float);
+        positions_size = (m_segments + 1) * 2 * sizeof(float);
     }
-    void move(const float x, const float y) override{
-        for (uint i = 0; i < (m_segments + 2) * 2; i += 2) {
-            positions[i] += x;     
-            positions[i + 1] += y; 
+
+    void move(const float x, const float y) override {
+        for (uint i = 0; i < (m_segments + 1) * 2; i += 2) {
+            positions[i] += x;
+            positions[i + 1] += y;
         }
     }
-    const uint get_positions_size()const override{
+
+    const uint get_positions_size() const override {
         return positions_size;
     }
-    const uint get_indices_size() const{
-        return m_segments + 2;
+
+    const uint get_indices_size() const {
+        return m_segments * 3;
     }
-    void calc_positions() override{
-        uint idx = 2;
+
+    void calc_positions() override {
         positions[0] = origin[0];
         positions[1] = origin[1];
-        
 
+        uint idx = 2;
         float theta;
-        for(int i = 0; i <= m_segments;++i){
+        for (int i = 0; i < m_segments; ++i) {
             theta = (TWO_PI * float(i)) / float(m_segments);
             float x = m_radius * cosf(theta);
             float y = m_radius * sinf(theta);
-            
+
             positions[idx++] = origin[0] + x;
             positions[idx++] = origin[1] + y;
-
         }
     }
 
-    const float* get_positions() const override{
-        return positions.get();
-    }
-    const uint* get_indices() const override{
-        return indices.get();
-    }
-    void init_positions() override{
-        positions = std::make_unique<float[]>((m_segments + 2) * 2);
+    const std::vector<float>& get_positions() const override {
+        return positions;
     }
 
-    void init_indices() override{
-        indices = std::make_unique<uint[]>(m_segments + 2);
-        indices[0] = 0;
-        for(int i = 1; i <= m_segments; i++){
-            indices[i] = i;
+    const std::vector<uint>& get_indices() const override {
+        return indices;
+    }
+
+    void init_positions() override {
+        positions.resize((m_segments + 1) * 2);
+    }
+
+    void init_indices() override {
+        indices.resize(m_segments * 3);
+        for (uint i = 0; i < m_segments; ++i) {
+            indices[i * 3] = 0;
+            indices[i * 3 + 1] = i + 1;
+            indices[i * 3 + 2] = (i + 1) % m_segments + 1;
         }
-        indices[m_segments + 1] = 1;
+
     }
 };
