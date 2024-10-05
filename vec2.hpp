b251c1236b6f4c0d75b94bd957941d0cb8616700
InvestedBrick <julianbrecht25@gmail.com>
2024-10-05 21:57:13 +0200

    Added == operator

---
+++ vec2.hpp
@@ -0,0 +1,34 @@
+struct vec2f{
+    float x;
+    float y;
+
+    vec2f(float pos_x, float pos_y){
+        x = pos_x;
+        y = pos_y;
+    }
+    bool operator==(const vec2f& other) const{
+        return x == other.x && y == other.y;
+    }
+    vec2f operator+(const vec2f& other) const{
+        return vec2f(x + other.x,y + other.y);
+    }
+    vec2f& operator+=(const vec2f& other) {
+        x += other.x;
+        y += other.y;
+        return *this; 
+    }
+    vec2f& operator-=(const vec2f& other) {
+        x -= other.x;
+        y -= other.y;
+        return *this; 
+    }
+    vec2f operator-(const vec2f& other) const{
+        return vec2f(x - other.x,y - other.y);
+    }
+    vec2f operator*(const float scalar) const{
+        return vec2f(x * scalar,y * scalar);
+    }
+    vec2f operator/(const float scalar) const{
+        return vec2f(x / scalar,y / scalar);
+    }
+};
