7aad5966e98f26cf002c923b2df7c751fca62a40
InvestedBrick <julianbrecht25@gmail.com>
2024-10-16 21:32:23 +0200

    Added op= operators

--- vec2.hpp
+++ vec2.hpp
@@ -25,6 +25,21 @@ struct vec2f{
         y -= other.y;
         return *this; 
     }
+    vec2f& operator*=(const vec2f& other) {
+        x *= other.x;
+        y *= other.y;
+        return *this; 
+    }
+    vec2f& operator/=(const vec2f& other) {
+        x /= other.x;
+        y /= other.y;
+        return *this; 
+    }
+    vec2f& operator/=(const float scalar) {
+        x /= scalar;
+        y /= scalar;
+        return *this; 
+    }
     vec2f operator-(const vec2f& other) const{
         return vec2f(x - other.x,y - other.y);
     }
