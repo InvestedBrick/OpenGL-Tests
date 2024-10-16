18d2bfb71f6fd7c906c62b40e16d2e71e2d22060
InvestedBrick <julianbrecht25@gmail.com>
2024-10-16 21:31:30 +0200

    Fixed Implementation

--- Barnes-Hut/Quadtree.hpp
+++ Barnes-Hut/Quadtree.hpp
@@ -2,59 +2,78 @@
 #include <memory>
 #include <vector>
 #include <cmath>
+#include <array>
 #include "../Simple_API/defines.hpp"
 #include "../Circle_Object.hpp"
+struct Quad{
+    Quad(vec2f center_,float size_){
+        center = center_;
+        size = size_;
+    }
+    Quad() = default;
+    vec2f center{0.0,0.0};
+    float size;
+    Quad initialize(std::vector<Circle_Object> &objects);
+    Quad into_quads(int quadrant) const {
+        Quad result = *this;
+        result.size *= 0.5f;
+        // Determine horizontal shift based on quadrant (0 or 1)
+        float x_shift = (quadrant % 2 == 0) ? -0.5f : 0.5f;
+        result.center.x += x_shift * result.size;
+        // Determine vertical shift based on quadrant (0 or 2)
+        float y_shift = (quadrant < 2) ? -0.5f : 0.5f;
+        result.center.y += y_shift * result.size;
+        return result;
+    
+    }
+    uint get_quadrant(vec2f pos){
+        return ((pos.y > center.y)) << 1 | (pos.x > center.x);
+    }
+    std::array<Quad,4> subdivide() const {
+        std::array<Quad, 4> quads;
+        for (int i = 0; i < 4; ++i) {
+            quads[i] = into_quads(i);
+        }
+        return quads;
+    }
+};
+
 class Quadtree{
 private:
-    enum Quadrant{
-        NW = 0,
-        NE = 1,
-        SW = 2,
-        SE = 3,
-    };
 
-    const float theta = 0.5; 
-    struct Quad{
-        Quad(vec2f center_,float size_){
-            center = center_;
-            size = size_;
-        }
+    float theta; 
+    float th_sq;
+    
+    struct Node{
+        
+        Node(uint next_, Quad q) : next(next_), quad(q) {};
         float mass = 0.f;
         vec2f center_of_mass{0.0,0.0};
-        std::unique_ptr<Quad> children[4] = {nullptr,nullptr,nullptr,nullptr};
-        vec2f center{0.0,0.0};
-        float size;
-        bool is_leaf() const {return children[0] == nullptr && children[1] == nullptr && children[2] == nullptr && children[3] == nullptr;}
+        uint children = 0;
+        uint next;
+        bool is_leaf() const {return children == 0;}
+        Quad quad;
     };
-    inline Quadrant get_quadrant(std::unique_ptr<Quad>& quad,vec2f& body_pos){
-        if (body_pos.y >= quad->center.y ){ // North
-            return body_pos.x >= quad->center.x ? Quadrant::NE : Quadrant::NW;
-        }else{ // South     
-            return body_pos.x >=  quad->center.x ? Quadrant::SE : Quadrant::SW;
-        }
-    }
-    void subdivide(std::unique_ptr<Quad>& quad);
-    void recursive_insert(std::unique_ptr<Quad>& quad, Circle_Object& object);
-    void print_quad_recursive(const std::unique_ptr<Quad>& quad, int level) const;
-    void apply_force_recursively(Circle_Object& object, std::unique_ptr<Quad>& quad);
-    vec2f calc_force(Circle_Object& object, std::unique_ptr<Quad>& quad);
+
+    uint subdivide(uint node);
+    vec2f calc_force(Circle_Object& object, uint node);
 
     float get_dist_squared(const vec2f& a, const vec2f& b){
         float x_sq = (b.x - a.x) * (b.x - a.x);
         float y_sq = (b.y - a.y) * (b.y - a.y);
         return  x_sq + y_sq;
     }
-    bool should_approximate(Circle_Object& object, std::unique_ptr<Quad>& quad){
-        return quad->size * quad->size / get_dist_squared(object.pos,quad->center_of_mass) < this->theta * this->theta;
+    bool should_approximate(Circle_Object& object, uint node){
+        return nodes[node].quad.size * nodes[node].quad.size / get_dist_squared(object.pos,nodes[node].center_of_mass) < this->theta * this->theta;
     }
+    std::vector<uint> parents;
+    std::vector<Node> nodes;
 public:
-    void print_tree() const;
-    std::unique_ptr<Quad> root = nullptr;
-    Quadtree() = default;
+    Quadtree(const float theta_) : theta(theta_){ th_sq = theta * theta;};
     Quadtree(const Quadtree* other) = delete; // dont need to copy quadtrees
     ~Quadtree() = default;
     void insert_body(Circle_Object& object);
-    void set_size(std::vector<Circle_Object>& objects);
     void apply_force(Circle_Object& object);
-    void set_numeric_size(vec2f center, float size);
+    void update_mass_centers();
+    void reset(Quad quad);
 };
