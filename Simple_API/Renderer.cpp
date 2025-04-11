7ebe209859fbfd25d8cf66a442a787be8dca4314
InvestedBrick <julianbrecht25@gmail.com>
2025-04-11 18:54:35 +0200

    Added draw_instanced

--- Simple_API/Renderer.cpp
+++ Simple_API/Renderer.cpp
@@ -16,3 +16,12 @@ void Renderer::draw(const VertexArray &vao, const IndexBuffer &ibo, const GlProg
     glCall(glDrawElements(mode, ibo.get_count(),GL_UNSIGNED_INT, nullptr));
 
 }
+
+void Renderer::draw_instanced(const VertexArray &vao,const IndexBuffer &ibo ,const GlProgram & program,GLenum mode, uint count) const
+{
+    program.bind();
+    vao.bind();
+    ibo.bind();
+
+    glCall(glDrawElementsInstanced(mode, ibo.get_count(), GL_UNSIGNED_INT, nullptr,count));
+}
