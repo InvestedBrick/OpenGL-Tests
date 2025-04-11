ad81296fd12229e875e525784843c4c07d980ef7
InvestedBrick <julianbrecht25@gmail.com>
2025-04-11 18:54:41 +0200

    Added draw_instanced

--- Simple_API/Renderer.hpp
+++ Simple_API/Renderer.hpp
@@ -9,5 +9,5 @@ class Renderer{
 public:
 void clear() const;
 void draw(const VertexArray& vao,const IndexBuffer& ibo,const GlProgram& program,GLenum mode = GL_TRIANGLES) const;  
-
+void draw_instanced(const VertexArray &vao,const IndexBuffer &ibo ,const GlProgram & program,GLenum mode, uint count) const;
 };
